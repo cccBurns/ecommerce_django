e7c87b85baaa65e7838dca9da0818badc93cdd50
christian burns <christianburns03@gmail.com>
2024-05-15 16:07:54 -0300

    miercoles 15 16hs

--- inicio/views.py
+++ inicio/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -11,7 +11,21 @@ def about(request):
     return render(request, 'about.html', {})
 
 def login_user(request):
-    return render(request, 'login.html', {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("Usuario y Contraseña Correcta"))
+            return redirect('home')   
+        else:
+            messages.success(request, ("Contraseña Incorrecta"))
+            return redirect('login')
+    else:        
+        return render(request, 'login.html', {})
 
 def logout_user(request):
-    return render(request, 'logout.html', {})
+    logout(request)
+    messages.success(request, ("Su cuenta se encuentra cerrada."))
+    return redirect('home')
