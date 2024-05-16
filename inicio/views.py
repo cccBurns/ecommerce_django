4d1fbdb5f644a41ac7a109078ecf509b1c0c5020
christian burns <christianburns03@gmail.com>
2024-05-16 11:10:46 -0300

    jueves 16, 11hs

--- inicio/views.py
+++ inicio/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 from django import forms
+
+def category(request, foo):
+    # Replace Hyphens with Spaces
+    foo = foo.replace('-', '')
+    # Grab the category from the url
+    try:
+        # Look up the Category
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products':products, 'category':category})
+
+    except:
+        messages.success(request, ("Esa categoria no existe"))
+        return redirect('home')
+    
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product':product})
 
 def home(request):
     products = Product.objects.all()
@@ -34,5 +53,21 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    
-     return render(request, 'register.html', {})
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # log in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Registro Exitoso"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Oops!, Intentalo Nuevamente"))
+            return redirect('register')
+            
+    else:            
+        return render(request, 'register.html', {'form':form})
