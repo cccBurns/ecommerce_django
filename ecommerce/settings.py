69560e819deb1febde44e13aced32662c3213bcb
christian burns <christianburns03@gmail.com>
2024-05-16 14:58:24 -0300

    jueves 16 15hs

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'inicio',
     'blog',
-    'login',     
+    'login',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
