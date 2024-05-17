97aa6a36991f0893932f22b073b7bd3cb05d6874
christian burns <christianburns03@gmail.com>
2024-05-17 10:20:53 -0300

    viernes 17, 10hs

--- inicio/urls.py
+++ inicio/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     path('product/<int:pk>', views.product, name='product'),
-    path('category/<str:foo>', views.category, name='category')
+    path('category/<str:foo>', views.category, name='category'),
+    path('category_summary/', views.category_summary, name='category_summary')
 ]
